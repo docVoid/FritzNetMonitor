430627e4ab56314bd6f9a7106209eb2e4dd32be8
doc.void <81415427+docVoid@users.noreply.github.com>
2025-05-06 17:50:02 +0200

    Update fritzreader.py

--- fritzreader.py
+++ fritzreader.py
@@ -1,10 +1,10 @@
 from fritzconnection.lib.fritzhosts import FritzHosts
 
 def get_connected_devices():
-    fh = FritzHosts(address='fritz.box', user='fritz6772', password='pfeil4967')
+    fh = FritzHosts(address='fritz.box', user='fritz6772', password='PASSWORD')
     connected = []
     for device in fh.get_hosts_info():
-        if device.get("status") == "1":  # 1 = aktiv / online
+        if device.get("status") == "1":  
             connected.append({
                 "name": device.get("name", "Unbekannt"),
                 "ip": device.get("ip"),
